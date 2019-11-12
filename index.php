c984e33050e580a7607f97d2de989177a1f26edb
James Downer <downer.james.256k@gmail.com>
2019-11-12 15:51:55 -0700

    Add index + composer

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Recipe Web App</title>
+    <meta charset="UTF-8"/>
+    <link rel="stylesheet" href="styles/top.css"/>
+  </head>
+  <body>
+    
+  </body>
+</html>
