97f9f17328d2bedad3ea642fc2d75364aba74d42
James Downer <downer.james.256k@gmail.com>
2019-11-13 15:10:49 -0700

    Add top bar & icons

--- index.php
+++ index.php
@@ -6,6 +6,23 @@
     <link rel="stylesheet" href="styles/top.css"/>
   </head>
   <body>
-    
+    <div class="wrapper">
+      <div class="menu-button" id="bt-menu">
+        <img src="assets/icons/hamburger.png" width="40" height="40"/>
+      </div>
+      
+      <div class="top-bar">
+        <div class="menu-bar" hidden>
+        </div>
+        
+        <div class="top-bar-button" id="bt-search">
+          <img src="assets/icons/search.png" width="40" height="40"/>
+        </div>
+      </div>
+      
+      <div class="account-button" id="bt-account">
+        <img src="assets/icons/user.png" width="40" height="40"/>
+      </div>
+    </div>
   </body>
 </html>
