fa4aed10e23a3b812507efe4fa9265c0c2a70efd
James Downer <downer.james.256k@gmail.com>
2019-11-23 02:26:39 -0700

    Add menu shade + move account button

--- src/topbar.js
+++ src/topbar.js
@@ -1,6 +1,6 @@
 let divMenu;
+let divMenuShade;
 let btMenu;
-let btAccount;
 
 let menuState = false;
 
@@ -14,21 +14,28 @@ function toggleMenu(state) {
   if (!menuState) {
     divMenu.style.left = `${0 - divMenu.offsetWidth}px`;
     btMenu.style.left = 0;
+    divMenuShade.hidden = true;
   } else {
     // one-time visibility toggle
     divMenu.style.visibility = 'visible';
+    
     divMenu.style.left = 0;
     btMenu.style.left = `${divMenu.offsetWidth - 1}px`;
+    divMenuShade.hidden = false;
   }
 }
 
 window.addEventListener('load', () => {
   divMenu = elemById('divMenu');
+  divMenuShade = elemById('divMenuShade');
   btMenu = elemById('btMenu');
-  btAccount = elemById('btAccount');
   
   btMenu.addEventListener('click', () => {
     toggleMenu();
+  });
+  
+  divMenuShade.addEventListener('click', () => {
+    toggleMenu(false);
   });
   
   toggleMenu(false);
--- views/topbar.php
+++ views/topbar.php
@@ -10,7 +10,18 @@ function showTopBar() {
         Home
       </div>
       <div class="menu-button-icon">
-        <img src="/assets/icons/home.png" alt="Home" width="30" height="30"/>
+        <img src="/assets/icons/home.png" width="30" height="30"/>
+      </div>
+    </div>
+    </a>
+    
+    <a href="#">
+    <div class="menu-link-button">
+      <div class="menu-link">
+        Account
+      </div>
+      <div class="menu-button-icon">
+        <img src="/assets/icons/user.png" width="30" height="30"/>
       </div>
     </div>
     </a>
@@ -21,7 +32,7 @@ function showTopBar() {
         Search
       </div>
       <div class="menu-button-icon">
-        <img src="/assets/icons/search.png" alt="Home" width="30" height="30"/>
+        <img src="/assets/icons/search.png" width="30" height="30"/>
       </div>
     </div>
     </a>
@@ -32,15 +43,13 @@ function showTopBar() {
   <img src="/assets/icons/hamburger.png" width="40" height="40"/>
 </div>
 
+<div class="menu-shade" id="divMenuShade" hidden></div>
+
 <div class="top-bar">
   <div class="menu-bar" hidden>
   </div>
   
   <label class="title" id="lblTitle">Recipe Web App</label>
-</div>
-
-<div class="button account-button" id="btAccount">
-  <img src="/assets/icons/user.png" width="40" height="40"/>
 </div>
 
 <script src="/src/topbar.js"></script>
