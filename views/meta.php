0b2c28b8357c7e9c1db0940f108d36f2d1ab2e4b
James Downer <downer.james.256k@gmail.com>
2019-11-22 22:10:40 -0700

    Add manifest for PWA (Add to Home Screen)

--- views/meta.php
+++ views/meta.php
@@ -7,6 +7,7 @@ function getMeta($title) {
   <meta name="og:image" content="https://cdn.pixabay.com/photo/2017/03/10/13/57/cooking-2132874_960_720.jpg"/>
   <link rel="stylesheet" href="styles/top.css"/>
   <link rel="shortcut icon" type="image/png" href="assets/icons/recipe.png"/>
+  <link rel="manifest" href="manifest.json"/>
 <?php
 }
 ?>
